89b25e0d6fe23d255520703a92519df060a7d85c
Helena R. S. D'Espindula <58571971+HelenaDEspindula@users.noreply.github.com>
2025-08-02 04:11:05 -0300

    Atualizar o testes.sh

--- conda-original/testes.sh
+++ conda-original/testes.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+INPUT_FILE="input_fsm-lite_OXA-23_OXA-24_020.txt"
+TIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
+LOG_DIR="logs"
+TMP_DIR="tmp"
+MONITOR_LOG="${LOG_DIR}/fsm_monitor_log_${TIMESTAMP}.txt"
+OUTPUT_LOG="${LOG_DIR}/fsm_output_log_${TIMESTAMP}.txt"
+TMP_FILES="${TMP_DIR}/fsm_tmp_files_${TIMESTAMP}"
+OUTPUT_RES="fsm_results_${TIMESTAMP}.txt.gz"
+SESSION_RUN="fsm_run"
+SESSION_MONITOR="fsm_monitor"
+INTERVAL_MONITOR=30
+
 # Caminho para o programa a ser testado
 PROGRAMA="fsm-lite"  # ou o nome/caminho correto
 
