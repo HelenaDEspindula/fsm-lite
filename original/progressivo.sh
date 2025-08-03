a0d58b84306bb6f941dba1d9b7746062ecc11aae
HelenaDEspindula <helena.despindula@gmail.com>
2025-08-03 15:24:03 -0300

    rodando os originais com variacao de paramentros

--- original/progressivo.sh
+++ original/progressivo.sh
@@ -3,55 +3,99 @@
 INPUT_FILE="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/input_fsm-lite_OXA-23_OXA-24_temp.txt"
 LISTA="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/input_fsm-lite_OXA-23_OXA-24_100.txt"
 LOG_DIR="logs"
-TMP_DIR="tmp"
 INTERVAL_MONITOR=30
 GENOMAS=(5 10 25 50 100)
 SMINUSCULO=6
-SMAIUSCULO=60
+SMAIUSCULO=(10 50 200 600)
 MMINUSCULO=1
-LOG_DIR="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/fsm-lite-temp"
-TMP_DIR="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/fsm-lite-results"
+TMP_DIR="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/fsm-lite-temp"
+RES_DIR="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/fsm-lite-results"
 
 
 PROGRAMA="./fsm-lite"
 
-for N in "${GENOMAS[@]}"; do
-  echo "============================="
-  echo "Testando com $N amostras..."
+# Criar pastas
+mkdir -p "$LOG_DIR"
+mkdir -p "$TMP_DIR"
+mkdir -p "$RES_DIR"
 
-  TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
-  MONITOR_LOG="${LOG_DIR}/fsm_monitor_log_${TIMESTAMP}.tsv"
-  OUTPUT_LOG="${LOG_DIR}/fsm_output_log_${TIMESTAMP}.txt"
-  TMP_FILES="${TMP_DIR}/fsm_tmp_files_${TIMESTAMP}"
-  OUTPUT_RES="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/fsm_results_${TIMESTAMP}.txt"
+for N in "${GENOMAS[@]}"; do
 
   # Criar sublista
   head -n "$N" "$LISTA" > "$INPUT_FILE"
 
-  echo "Rodando fsm-lite para $N amostras." > "$MONITOR_LOG"
-  echo -e "timestamp\tcpu\tmem\tvsz\trss" >> "$MONITOR_LOG"
+  for J in "${SMAIUSCULO[@]}"; do
   
-  echo "Rodando fsm-lite para $N amostras." > "$OUTPUT_RES"
-
-  echo "Rodando fsm-lite para $N amostras..."
+    echo "============================="
+    echo "Testando com TXT: $N amostras com $J de maximo as ${TIMESTAMP}"
   
-  # Executar fsm-lite em background
-  ( /usr/bin/time -v "$PROGRAMA" -l "$INPUT_FILE" -s $SMINUSCULO -S $SMAIUSCULO -m $MMINUSCULO --debug -v -t "$TMP_FILES" >> "$OUTPUT_RES" 2> "$OUTPUT_LOG" ) &
-  FSM_PID=$!
-
-  echo "Monitorando PID: $FSM_PID"
-
-  # Monitorar enquanto o processo estiver rodando
-  while kill -0 "$FSM_PID" 2>/dev/null; do
-    timestamp=$(date +%s)
-    ps -p "$FSM_PID" -o %cpu,%mem,vsz,rss --no-headers | \
-      awk -v t="$timestamp" '{print t"\t"$1"\t"$2"\t"$3"\t"$4}' >> "$MONITOR_LOG"
-    pidstat -h -r -u -p $FSM_PID 1 1 | awk -v t="$timestamp" 'NR==4 {print t"\t"$8"\t"$9"\t"$10"\t"$11}' >> "$MONITOR_LOG"
-    sleep "$INTERVAL_MONITOR"
+    TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+    MONITOR_LOG="${LOG_DIR}/fsm_monitor_log_${TIMESTAMP}.tsv"
+    OUTPUT_LOG="${LOG_DIR}/fsm_output_log_${TIMESTAMP}.txt"
+    TMP_FILES="${TMP_DIR}/fsm_tmp_files_${TIMESTAMP}"
+    OUTPUT_RES="${RES_DIR}/fsm_results_${TIMESTAMP}.txt"
+  
+    
+  
+    echo "Rodando fsm-lite TXT: para $N amostras com $J de maximo as ${TIMESTAMP}." > "$MONITOR_LOG"
+    echo -e "timestamp\tcpu\tmem\tvsz\trss" >> "$MONITOR_LOG"
+    
+    echo "Rodando fsm-lite TXT: para $N amostras com $J de maximo as ${TIMESTAMP}." > "$OUTPUT_RES"
+  
+    echo "Rodando fsm-lite TXT: para $N amostras com $J de maximo as ${TIMESTAMP}."
+    
+    # Executar fsm-lite em background
+    ( /usr/bin/time -v "$PROGRAMA" -l "$INPUT_FILE" -s $SMINUSCULO -S $SMAIUSCULO -m $MMINUSCULO --debug -v -t "$TMP_FILES" >> "$OUTPUT_RES" 2> "$OUTPUT_LOG" ) &
+    FSM_PID=$!
+  
+    echo "Monitorando PID: $FSM_PID"
+  
+      # Monitorar enquanto o processo estiver rodando
+      while kill -0 "$FSM_PID" 2>/dev/null; do
+        timestamp=$(date +%s)
+        ps -p "$FSM_PID" -o %cpu,%mem,vsz,rss --no-headers | \
+          awk -v t="$timestamp" '{print t"\t"$1"\t"$2"\t"$3"\t"$4}' >> "$MONITOR_LOG"
+        pidstat -h -r -u -p $FSM_PID 1 1 | awk -v t="$timestamp" 'NR==4 {print t"\t"$8"\t"$9"\t"$10"\t"$11}' >> "$MONITOR_LOG"
+        sleep "$INTERVAL_MONITOR"
+      done
+    
+    echo "============================="
+    echo "Testando com GZ: $N amostras com $J de maximo as ${TIMESTAMP}"
+  
+    TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+    MONITOR_LOG="${LOG_DIR}/fsm_monitor_log_${TIMESTAMP}.tsv"
+    OUTPUT_LOG="${LOG_DIR}/fsm_output_log_${TIMESTAMP}.txt"
+    TMP_FILES="${TMP_DIR}/fsm_tmp_files_${TIMESTAMP}"
+    OUTPUT_RES="${RES_DIR}/fsm_results_${TIMESTAMP}.txt.gz"
+  
+    
+  
+    echo "Rodando fsm-lite GZ: para $N amostras com $J de maximo as ${TIMESTAMP}." > "$MONITOR_LOG"
+    echo -e "timestamp\tcpu\tmem\tvsz\trss" >> "$MONITOR_LOG"
+    
+    echo "Rodando fsm-lite GZ: para $N amostras com $J de maximo as ${TIMESTAMP}." > "$OUTPUT_RES"
+  
+    echo "Rodando fsm-lite GZ: para $N amostras com $J de maximo as ${TIMESTAMP}."
+    
+    # Executar fsm-lite em background
+    ( ( /usr/bin/time -v "$PROGRAMA" -l "$INPUT_FILE" -s $SMINUSCULO -S $SMAIUSCULO -m $MMINUSCULO --debug -v -t "$TMP_FILES" | gzip >> "$OUTPUT_RES" ) 2> "$OUTPUT_LOG" ) &
+    FSM_PID=$!
+  
+    echo "Monitorando PID: $FSM_PID"
+  
+      # Monitorar enquanto o processo estiver rodando
+      while kill -0 "$FSM_PID" 2>/dev/null; do
+        timestamp=$(date +%s)
+        ps -p "$FSM_PID" -o %cpu,%mem,vsz,rss --no-headers | \
+          awk -v t="$timestamp" '{print t"\t"$1"\t"$2"\t"$3"\t"$4}' >> "$MONITOR_LOG"
+        pidstat -h -r -u -p $FSM_PID 1 1 | awk -v t="$timestamp" 'NR==4 {print t"\t"$8"\t"$9"\t"$10"\t"$11}' >> "$MONITOR_LOG"
+        sleep "$INTERVAL_MONITOR"
+      done
+    
   done
 
   wait "$FSM_PID"
-  echo "Finalizado teste com $N amostras."
+  echo "Finalizado testes com $N amostras com $J de maximo as ${TIMESTAMP}."
 done
 
 echo "Todos os testes foram concluídos."
