38ac8b00d04a17df0111f30ff435e632f957e6cf
Helena R. S. D'Espindula <58571971+HelenaDEspindula@users.noreply.github.com>
2025-08-14 03:51:41 -0300

    Atualizar o paralelo.sh

--- v2_6/paralelo.sh
+++ v2_6/paralelo.sh
@@ -1,74 +1,56 @@
 #!/bin/bash
 
+# Corrige o PATH para garantir acesso aos comandos mesmo dentro do Conda
+export PATH="$CONDA_PREFIX/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+
 INPUT_FILE="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/input_fsm-lite_OXA-23_OXA-24_temp.txt"
 LISTA="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/input_fsm-lite_OXA-23_OXA-24_todos.txt"
 LOG_DIR="logs"
 INTERVAL_MONITOR=30
-GENOMAS=(25 100 250)
+GENOMAS=5
 SMINUSCULO=6
 SMAIUSCULO=600
 MMINUSCULO=1
-VERSION="2_6"
+VERSION="2_5"
 TMP_DIR="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/fsm-lite-temp"
 RES_DIR="/home/helena.despindula/LACTAS-HELISSON-01/Helena-stuff/fsm-lite-results/${VERSION}"
 PROGRAMA="./fsm-lite"
-MAX_PARALLEL=3
 
 mkdir -p "$LOG_DIR/monitor" "$LOG_DIR/output" "$TMP_DIR" "$RES_DIR"
 
 for N in "${GENOMAS[@]}"; do
-
-  echo "Extraindo os $N primeiros genomas da lista para processamento..."
   head -n "$N" "$LISTA" > "$INPUT_FILE"
 
-  job_count=0
+  for J in "${SMAIUSCULO[@]}"; do
+    TIMESTAMP=$($CONDA_PREFIX/bin/date +%Y-%m-%d_%H-%M-%S)
+    MONITOR_LOG="${LOG_DIR}/monitor/fsm_monitor_log_v${VERSION}_${N}genomas_${J}_max_TXT--${TIMESTAMP}.txt"
+    OUTPUT_LOG="${LOG_DIR}/output/fsm_output_log_v${VERSION}_${N}genomas_${J}_max_TXT--${TIMESTAMP}.txt"
+    TMP_FILES="${TMP_DIR}/fsm_tmp_files_v${VERSION}_${N}genomas_${J}_max_TXT--${TIMESTAMP}"
+    OUTPUT_RES="${RES_DIR}/fsm_results_v${VERSION}_${N}genomas_${J}_max_TXT--${TIMESTAMP}.txt"
 
-  while IFS=$'\t' read -r ID PATH; do
-    TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+    echo "Rodando fsm-lite v${VERSION} saída TXT para $N amostras com $J máximo às ${TIMESTAMP}." | tee "$MONITOR_LOG" > "$OUTPUT_LOG"
+    echo -e "timestamp\tcpu\tmem\tvsz\trss" >> "$MONITOR_LOG"
 
-    MONITOR_LOG="${LOG_DIR}/monitor/fsm_monitor_log_v${VERSION}_${ID}_${N}_genomas_TXT--${TIMESTAMP}.txt"
-    OUTPUT_LOG="${LOG_DIR}/output/fsm_output_log_v${VERSION}_${ID}_${N}_genomas_TXT--${TIMESTAMP}.txt"
-    TMP_FILES="${TMP_DIR}/fsm_tmp_files_v${VERSION}_${ID}_${N}_genomas_TXT--${TIMESTAMP}"
-    OUTPUT_RES="${RES_DIR}/fsm_results_v${VERSION}_${ID}_${N}_genomas_TXT--${TIMESTAMP}.txt"
+    ( /usr/bin/time -v "$PROGRAMA" -l "$INPUT_FILE" -s $SMINUSCULO -S $SMAIUSCULO -m $MMINUSCULO -t "$TMP_FILES" > "$OUTPUT_RES" 2>> "$OUTPUT_LOG" ) &
+    FSM_PID=$!
 
-    echo -e "${ID}\t${PATH}" > "$TMP_FILES.list"
+    echo "[INFO] Monitorando PID: $FSM_PID"
 
-    echo "[INFO] Executando genoma $ID com saída em $OUTPUT_RES"
+    while kill -0 "$FSM_PID" 2>/dev/null; do
+      timestamp=$($CONDA_PREFIX/bin/date +%s)
+      $CONDA_PREFIX/bin/ps -p "$FSM_PID" -o %cpu,%mem,vsz,rss --no-headers | \
+        $CONDA_PREFIX/bin/awk -v t="$timestamp" '{print t"\t"$1"\t"$2"\t"$3"\t"$4}' >> "$MONITOR_LOG"
 
-    (
-      echo "Rodando fsm-lite v${VERSION} genoma ${ID}: $TIMESTAMP." > "$MONITOR_LOG"
-      echo -e "timestamp\tcpu\tmem\tvsz\trss" >> "$MONITOR_LOG"
+      $CONDA_PREFIX/bin/pidstat -h -r -u -p $FSM_PID 1 1 | \
+        $CONDA_PREFIX/bin/awk -v t="$timestamp" 'NR==4 {print t"\t"$8"\t"$9"\t"$10"\t"$11}' >> "$MONITOR_LOG"
 
-      echo "Rodando fsm-lite v${VERSION} genoma ${ID}: $TIMESTAMP." > "$OUTPUT_LOG"
+      $CONDA_PREFIX/bin/sleep "$INTERVAL_MONITOR"
+    done
 
-      # Executar o programa
-      ( /usr/bin/time -v "$PROGRAMA" -l "$TMP_FILES.list" -s $SMINUSCULO -S $SMAIUSCULO -m $MMINUSCULO -t "$TMP_FILES" > "$OUTPUT_RES" 2>> "$OUTPUT_LOG" ) &
-      FSM_PID=$!
-
-      echo "[INFO] Monitorando PID $FSM_PID para genoma $ID"
-
-      while kill -0 "$FSM_PID" 2>/dev/null; do
-        timestamp=$(date +%s)
-        ps -p "$FSM_PID" -o %cpu,%mem,vsz,rss --no-headers | \
-          awk -v t="$timestamp" '{print t"\t"$1"\t"$2"\t"$3"\t"$4}' >> "$MONITOR_LOG"
-        pidstat -h -r -u -p "$FSM_PID" 1 1 | \
-          awk -v t="$timestamp" 'NR==4 {print t"\t"$8"\t"$9"\t"$10"\t"$11}' >> "$MONITOR_LOG"
-        sleep "$INTERVAL_MONITOR"
-      done
-
-      echo "[INFO] Genoma $ID finalizado."
-    ) &
-
-    ((job_count++))
-
-    if (( job_count % MAX_PARALLEL == 0 )); then
-      wait
-    fi
-
-  done < "$INPUT_FILE"
-
-  wait
-  echo "[INFO] Finalizado grupo de $N genomas."
+    wait "$FSM_PID"
+    echo "[INFO] Finalizado testes com $N amostras com $J máximo às ${TIMESTAMP}."
+  done
 done
 
+echo "============================="
 echo "Todos os testes foram concluídos."
